e76fdb9e268d27b86449a898ec265544cd7df886
Robin Le Bihan <Robin Le Bihan>
2016-04-05 18:03:09 +0200

    Commit for Arthur

---
+++ Assets/Réseau/MOHSNetworkManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+
+public class MOHSNetworkManager : NetworkManager {
+    public void StartupHost()
+    {
+        SetPort();
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void JoinGame()
+    {
+        SetIPAdress();
+        SetPort();
+        NetworkManager.singleton.StartClient();
+    }
+
+    void SetIPAdress()
+    {
+        string ipAdress = GameObject.Find("InputFieldIPAdress").transform.FindChild("Text").GetComponent<Text>().text;
+        NetworkManager.singleton.networkAddress = ipAdress;
+    }
+
+    void SetPort()
+    {
+        NetworkManager.singleton.networkPort = 7777;
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (level == 0)
+            SetupMenuSceneButtons();
+    }
+
+    void SetupMenuSceneButtons()
+    {
+        GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.AddListener(StartupHost);
+
+        GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.AddListener(JoinGame);
+    }
+}
--- Assets/Réseau/Scripts/Player_NetworkSetup.cs
+++ Assets/Réseau/Scripts/Player_NetworkSetup.cs
@@ -16,9 +16,9 @@ public class Player_NetworkSetup : NetworkBehaviour
     {
         if (isLocalPlayer)
         {
-            GameObject.Find("Scene Camera").SetActive(false);
+            //GameObject.Find("Scene Camera").SetActive(false);
             GameObject.Find("SoundGen").GetComponent<Test_Proc>().enabled = true;
-            GameObject.Find("MenuMusic").SetActive(false);
+            //GameObject.Find("MenuMusic").SetActive(false);
             GameObject.Find("PlayerContain").GetComponent<PlayerContain>().player_obj = gameObject;
             GetComponent<CharacterController>().enabled = true;
             GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;

4032be10b940f9a7dff273eed2fb3552c0008550
Hedgestock <pamarthur@free.fr>
2016-04-06 12:59:24 +0100

    DigiCode fixed

--- Assets/Level Design/Doors/Scripts/DigiCode.cs
+++ Assets/Level Design/Doors/Scripts/DigiCode.cs
@@ -41,7 +41,7 @@ public class DigiCode : MonoBehaviour {
         {
             elapsed = 0;
             activated = !activated;
-            other.gameObject.GetComponentInChildren<Canvas>(false).enabled = activated;
+            other.gameObject.GetComponentInChildren<Canvas>().enabled = activated;
             other.gameObject.GetComponent<FirstPersonController>().enabled = !activated;
             Cursor.visible = activated;
             if (activated)

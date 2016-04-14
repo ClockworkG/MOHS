1cbbcdc709f82c734a8f63341bd6cf071ac5eee3
Hedgestock <pamarthur@free.fr>
2016-04-14 14:43:52 +0200

    Menu

--- Assets/Level Design/Ladders/Scripts/Ladder.cs
+++ Assets/Level Design/Ladders/Scripts/Ladder.cs
@@ -6,7 +6,6 @@ public class Ladder : MonoBehaviour {
     public CharacterController character_controller;
     public bool inside = false;
     public float height_factor = 3.2f;
-    
 
     void FixedUpdate()
     {

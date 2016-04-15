3b616a97fb066f2675100f3f4ef9fe463aca34d6
Hedgestock <pamarthur@free.fr>
2016-04-15 22:31:32 +0200

    CA MARCHE ON PEUT ANIMER UNe CAMERA !!!

--- Assets/Animations/Camera/CameraAnimation.cs
+++ Assets/Animations/Camera/CameraAnimation.cs
@@ -4,13 +4,38 @@ using System.Collections;
 public class CameraAnimation : MonoBehaviour {
     Animator Anim;
     int PlayHash = Animator.StringToHash("Play");
+    int SettingsHash = Animator.StringToHash("Settings");
+    int QuitHash = Animator.StringToHash("Quit");
+    int BonusHash = Animator.StringToHash("Bonus");
     // Use this for initialization
     void Start () {
         Anim = GetComponent<Animator>();
     }
-	
-	public void You_Play()
+	public void toSettings()
+    {
+        Anim.SetBool(SettingsHash, true);
+    }
+
+    public void toBonus()
+    {
+        Anim.SetBool(BonusHash, true);
+    }
+
+    public void toQuit()
+    {
+        Anim.SetBool(QuitHash, true);
+    }
+
+	public void toPlay()
     {
         Anim.SetBool(PlayHash, true);
+    }
+
+    public void toMain()
+    {
+        //Anim.SetBool(PlayHash, false);
+        Anim.SetBool(SettingsHash, false);
+        Anim.SetBool(BonusHash, false);
+        Anim.SetBool(QuitHash, false);
     }
 }

64fd5cbdafa16e3acb259ad1cf1696e1be6f7f32
Hedgestock <pamarthur@free.fr>
2016-05-03 15:49:20 +0200

    ON NE TWERK PLUS !!!

--- Assets/Animations/Character/CharacterAnimationScript.cs
+++ Assets/Animations/Character/CharacterAnimationScript.cs
@@ -10,6 +10,7 @@ public class CharacterAnimationScript : NetworkBehaviour
     int HorizontalHash = Animator.StringToHash("Horizontal");
     int VerticalHash = Animator.StringToHash("Vertical");
     int SprintHash = Animator.StringToHash("Sprint");
+    int WalkHash = Animator.StringToHash("Walk");
     // Use this for initialization
     void Start()
     {
@@ -24,6 +25,10 @@ public class CharacterAnimationScript : NetworkBehaviour
             return;
         Horizontal = Input.GetAxis("Horizontal");
         Vertical = Input.GetAxis("Vertical");
+        if (Horizontal!=0 || Vertical!=0)
+            Anim.SetBool(WalkHash, true);
+        else
+            Anim.SetBool(WalkHash, false);
         Anim.SetFloat(VerticalHash, Vertical);
         Anim.SetFloat(HorizontalHash, Horizontal);
         if (Input.GetKey(KeyCode.LeftShift) && Vertical == 1.0)

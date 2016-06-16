7eca2aeaed31640eae86701723ce8d9491bc3ac0
Robin Le Bihan <robin.le_bihan@epita.fr>
2016-06-16 12:56:19 +0200

    Player name

--- Assets/Interfaces/Scripts/ClientLobby.cs
+++ Assets/Interfaces/Scripts/ClientLobby.cs
@@ -1,7 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Collections;
 
 public class ClientLobby : MonoBehaviour {
+    public string player_name;
+    public InputField input;
 
+    void Start()
+    {
+        if (PlayerPrefs.GetString("Name", " ") == " ")
+        {
+            PlayerPrefs.SetString("Name", "Manuel");
+            player_name = "Manuel";
+        }
+
+        else
+        {
+            player_name = PlayerPrefs.GetString("Name");
+            input.text = PlayerPrefs.GetString("Name");
+        }
+    }
+
+    void Update()
+    {
+        player_name = input.text;
+        PlayerPrefs.SetString("Name", player_name);
+    }
 }
